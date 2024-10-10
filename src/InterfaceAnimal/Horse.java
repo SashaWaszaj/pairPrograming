614bff3058282749dac420fc8a6448dd550e7230
Stefan Vallet <stefanvallet99@gmail.com>
2024-10-10 12:44:45 -0300

    Create Horse.java

---
+++ src/InterfaceAnimal/Horse.java
@@ -0,0 +1,46 @@
+package InterfaceAnimal;
+
+import java.util.Date;
+
+public class Horse implements Animal {
+    private String species = "Caballo";
+    private String breed;
+    private String name;
+    private int birthYear;
+
+    public Horse(String breed, String name, int birthYear) {
+        this.breed = breed;
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    @Override
+    public String getSpecie() {
+        return this.species;
+    }
+
+    @Override
+    public String getBreed() {
+        return this.breed;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBirthYear() {
+        return this.birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal: " + species + ", breed: " + breed + ", name: " + name + ", edad: " + this.getAge();
+    }
+
+    @Override
+    public int getAge() {
+        return new Date().getYear() + 1900 - birthYear;
+    }
+}
