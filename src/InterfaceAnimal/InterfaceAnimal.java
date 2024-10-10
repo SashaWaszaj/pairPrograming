6be0dec3cbbff5e128b595b8543ec36d186828de
Stefan Vallet <stefanvallet99@gmail.com>
2024-10-10 12:52:15 -0300

    Implement mamiferos
    add horse & dog to the ZooArea

--- src/InterfaceAnimal/InterfaceAnimal.java
+++ src/InterfaceAnimal/InterfaceAnimal.java
@@ -1,18 +1,17 @@
 package InterfaceAnimal;
 
 public class InterfaceAnimal {
-    public static void prueba(){
-
-
+    public static void prueba() {
         Zoo botanico = new Zoo("botanico");
         ZooArea felinos = new ZooArea("felinos", 2);
         ZooArea reptiles = new ZooArea("reptiles", 3);
+        ZooArea mamiferos = new ZooArea("mamiferos", 4);
 
         botanico.addArea(felinos);
         botanico.addArea(reptiles);
+        botanico.addArea(mamiferos);
 
-
-        Animal cat = new Cat("carey", "micha", 2022);
+        Animal cat = new Cat("carey", "Micha", 2022);
         System.out.println(cat.toString());
         felinos.addAnimal(cat);
 
@@ -24,13 +23,20 @@ public class InterfaceAnimal {
         System.out.println(tigerCat.toString());
         felinos.addAnimal(tigerCat);
 
-        System.out.println(felinos.toString());
+        Animal horse = new Horse("arabe", "Tiro al blanco", 2015);
+        System.out.println(horse.toString());
+        mamiferos.addAnimal(horse);
 
+        Animal dog = new Dog("delmer", "Firulais", 2019);
+        System.out.println(dog.toString());
+        mamiferos.addAnimal(dog);
+
+        System.out.println(felinos.toString());
+        System.out.println(mamiferos.toString());
 
         int numeroAnimales = 1;
-        System.out.println("Areas con mayor a " + numeroAnimales+" de numero de animales");
-
-        botanico.getAreas().stream().filter(area -> area.getNumberOfAnimals() > numeroAnimales).forEach(area->{
+        System.out.println("Areas con mayor a " + numeroAnimales + " de numero de animales");
+        botanico.getAreas().stream().filter(area -> area.getNumberOfAnimals() > numeroAnimales).forEach(area -> {
             System.out.println(area.toString());
         });
     }
