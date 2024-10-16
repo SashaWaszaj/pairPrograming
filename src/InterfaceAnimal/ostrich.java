cfe92c1866669969510e4908f522922ea661fa76
kyrianWVDP <kyrianWeiss.vdP@gmail.com>
2024-10-16 14:14:12 -0300

    Update ostrich.java

--- src/InterfaceAnimal/ostrich.java
+++ src/InterfaceAnimal/ostrich.java
@@ -1,17 +1,46 @@
-package Animal;
+package InterfaceAnimal;
 
-public class ostrich extends Animal {
-    public ostrich(String breed, String name) {
-        super(name, breed);
+import java.time.LocalDate;
+
+public class Ostrich implements Animal {
+    private String species = "Avestruz"; // Ostrich in Spanish
+    private String breed;
+    private String name;
+    private int birthYear;
+
+    public Ostrich(String breed, String name, int birthYear) {
+        this.breed = breed;
+        this.name = name;
+        this.birthYear = birthYear;
     }
 
     @Override
-    public void makeSound() {
-        System.out.println("Hoot!");
+    public String getSpecie() {
+        return this.species;
+    }
+
+    @Override
+    public String getBreed() {
+        return this.breed;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getBirthYear() {
+        return this.birthYear;
     }
 
     @Override
     public String toString() {
-        return "ostrich - Name: " + name + ", Breed: " + breed;
+        return "Animal: " + species + ", breed: " + breed + ", name: " + name + ", edad: " + this.getAge();
+    }
+
+    @Override
+    public int getAge() {
+        return LocalDate.now().getYear() - birthYear; // Calculate age using current year
     }
 }
